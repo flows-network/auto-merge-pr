0e87d2507a8571c471c88f972f8ecb957ef97327
Ji Chen <jaykchen@gmail.com>
2023-03-07 16:57:30 -0500

    use hashmap to track reviewers who commented lgtm

--- src/auto-merge-pr.rs
+++ src/auto-merge-pr.rs
@@ -5,47 +5,102 @@ use http_req::{
     uri::Uri,
 };
 use slack_flows::send_message_to_channel;
+use std::collections::HashMap;
 use std::env;
 use tokio::*;
 #[no_mangle]
 #[tokio::main(flavor = "current_thread")]
 pub async fn run() -> anyhow::Result<()> {
+    let mut pr_review_tracker: HashMap<String, usize> = HashMap::new();
+    let lead_reviewer_list = vec!["jaykchen".to_string(), "amiiiiii830".to_string()];
+
     listen_to_event(
         "jaykchen",
         "vitesse-lite",
         vec!["pull_request", "pull_request_review"],
-        handler,
+        |payload| handler(payload, &mut pr_review_tracker, &lead_reviewer_list),
     )
     .await;
 
     Ok(())
 }
 
+pub fn track_lgtm(
+    review_text: &str,
+    reviewer_login: &str,
+    lead_reviewer_list: &Vec<String>,
+    pull_request_id: &str,
+    pr_review_tracker: &mut HashMap<String, usize>,
+) {
+    if lead_reviewer_list.contains(&reviewer_login.to_string())
+        && review_text.to_lowercase().contains("lgtm")
+    {
+        pr_review_tracker
+            .entry(pull_request_id.to_string())
+            .and_modify(|e| *e += 1)
+            .or_insert(1usize);
+    }
+}
+
 // comments_url = https://api.github.com/repos/jaykchen/vitesse-lite/issues/7/comments
-async fn handler(payload: EventPayload) {
+async fn handler(
+    payload: EventPayload,
+    pr_review_tracker: &mut HashMap<String, usize>,
+    lead_reviewer_list: &Vec<String>,
+) {
     match payload {
         EventPayload::PullRequestEvent(e) => {
-            send_message_to_channel(
-                "ik8",
-                "step_1",
-                serde_json::to_string(&e).unwrap_or("pull failed".to_string()),
-            );
+            send_message_to_channel("ik8", "step_1", "a pr was filed".to_string());
         }
 
         EventPayload::PullRequestReviewEvent(e) => {
-            send_message_to_channel(
-                "ik8",
-                "step_2",
-                serde_json::to_string(&e).unwrap_or("pr review failed".to_string()),
+            let pull_request_id: String = e.pull_request.id.to_string();
+            let review = e.review;
+            let review_text = review.body_text.unwrap_or("".to_string());
+            let reviewer_login: String = review.user.expect("no user found ").login;
+
+            track_lgtm(
+                &review_text,
+                &reviewer_login,
+                lead_reviewer_list,
+                &pull_request_id,
+                pr_review_tracker,
             );
+            let text: String = pr_review_tracker
+                .iter()
+                .map(|(a, b)| format!("{}: {}", a, b))
+                .collect::<Vec<String>>()
+                .join("; ");
+
+            send_message_to_channel("ik8", "step_2", text);
+            // send_message_to_channel(
+            //     "ik8",
+            //     "step_2",
+            //     serde_json::to_string(&e).unwrap_or("pr review failed".to_string()),
+            // );
         }
 
         EventPayload::PullRequestReviewCommentEvent(e) => {
-            send_message_to_channel(
-                "ik8",
-                "step_3",
-                serde_json::to_string(&e).unwrap_or("pr review comment failed".to_string()),
+            let pull_request_id: String = e.pull_request.id.to_string();
+            let comment = e.comment;
+            let review_text = comment.body_text.unwrap_or("".to_string());
+            let reviewer_login: String = comment.user.login;
+
+            track_lgtm(
+                &review_text,
+                &reviewer_login,
+                lead_reviewer_list,
+                &pull_request_id,
+                pr_review_tracker,
             );
+
+            let text: String = pr_review_tracker
+                .iter()
+                .map(|(a, b)| format!("{}: {}", a, b))
+                .collect::<Vec<String>>()
+                .join("; ");
+
+            send_message_to_channel("ik8", "step_3", text);
         }
 
         _ => (),
