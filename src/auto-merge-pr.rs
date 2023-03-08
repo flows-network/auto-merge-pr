472bce3a037d5b454df3207ee85f3d141383727f
Ji Chen <jaykchen@gmail.com>
2023-03-08 15:31:27 -0500

    archive track_lgtm function

--- src/auto-merge-pr.rs
+++ src/auto-merge-pr.rs
@@ -11,43 +11,65 @@ use tokio::*;
 #[no_mangle]
 #[tokio::main(flavor = "current_thread")]
 pub async fn run() -> anyhow::Result<()> {
-    let mut pr_review_tracker: HashMap<String, usize> = HashMap::new();
+    // let mut pr_review_tracker: HashMap<String, usize> = HashMap::new();
     let lead_reviewer_list = vec!["jaykchen".to_string(), "amiiiiii830".to_string()];
+    let owner = "jaykchen";
+    let repo = "vitesse-lite";
 
     listen_to_event(
-        "jaykchen",
-        "vitesse-lite",
+        owner,
+        repo,
         vec!["pull_request", "pull_request_review"],
-        |payload| handler(payload, &mut pr_review_tracker, &lead_reviewer_list),
+        |payload| handler(payload, &lead_reviewer_list),
     )
     .await;
 
     Ok(())
 }
 
-pub fn track_lgtm(
-    review_text: &str,
-    reviewer_login: &str,
-    lead_reviewer_list: &Vec<String>,
-    pull_request_id: &str,
-    pr_review_tracker: &mut HashMap<String, usize>,
-) {
-    if lead_reviewer_list.contains(&reviewer_login.to_string())
-        && review_text.to_lowercase().contains("lgtm")
-    {
-        pr_review_tracker
-            .entry(pull_request_id.to_string())
-            .and_modify(|e| *e += 1)
-            .or_insert(1usize);
-    }
-}
+// pub fn track_lgtm(
+//     review_text: &str,
+//     reviewer_login: &str,
+//     lead_reviewer_list: &Vec<String>,
+//     pull_request_id: &str,
+//     pr_review_tracker: &mut HashMap<String, usize>,
+// ) {
+//     if lead_reviewer_list.contains(&reviewer_login.to_string())
+//         && review_text.to_lowercase().contains("lgtm")
+//     {
+//         pr_review_tracker
+//             .entry(pull_request_id.to_string())
+//             .and_modify(|e| *e += 1)
+//             .or_insert(1usize);
+//     }
+// }
 
 // comments_url = https://api.github.com/repos/jaykchen/vitesse-lite/issues/7/comments
-async fn handler(
-    payload: EventPayload,
-    pr_review_tracker: &mut HashMap<String, usize>,
-    lead_reviewer_list: &Vec<String>,
-) {
+async fn handler(owner: &str, repo: &str, lead_reviewer_list: &Vec<String>, payload: EventPayload) {
+    let mut count = 0;
+    let octo = get_octo(Some(String::from(owner))).await;
+    let review_page = octo.pulls(owner, repo).get(pull_number).await.unwrap();
+
+    for item in review_page.items {
+        let reviewer_login = item.user.unwrap().login;
+        let review_text = item.body.unwrap();
+
+        if lead_reviewer_list.contains(&reviewer_login.to_string())
+            && review_text.to_lowercase().contains("lgtm")
+        {
+            count += 1;
+        }
+        if count >= 2 {
+            // merge pr
+            let merge_result = octo.pulls(owner, repo).merge(pull_number).await.unwrap();
+            send_message_to_channel("ik8", "step_4", "pr merged".to_string());
+        }
+    }
+
+    // let pr_first_message = page.body.unwrap();
+    // let issue = octo..issues("jaykchen", "vitesse-lite").get(17).await.unwrap();
+    // let pr_first_message = page.body.unwrap();
+
     match payload {
         EventPayload::PullRequestEvent(e) => {
             send_message_to_channel("ik8", "step_1", "a pr was filed".to_string());
@@ -58,19 +80,6 @@ async fn handler(
             let review = e.review;
             let review_text = review.body_text.unwrap_or("".to_string());
             let reviewer_login: String = review.user.expect("no user found ").login;
-
-            track_lgtm(
-                &review_text,
-                &reviewer_login,
-                lead_reviewer_list,
-                &pull_request_id,
-                pr_review_tracker,
-            );
-            let text: String = pr_review_tracker
-                .iter()
-                .map(|(a, b)| format!("{}: {}", a, b))
-                .collect::<Vec<String>>()
-                .join("; ");
 
             send_message_to_channel("ik8", "step_2", text);
             // send_message_to_channel(
@@ -86,58 +95,9 @@ async fn handler(
             let review_text = comment.body_text.unwrap_or("".to_string());
             let reviewer_login: String = comment.user.login;
 
-            track_lgtm(
-                &review_text,
-                &reviewer_login,
-                lead_reviewer_list,
-                &pull_request_id,
-                pr_review_tracker,
-            );
-
-            let text: String = pr_review_tracker
-                .iter()
-                .map(|(a, b)| format!("{}: {}", a, b))
-                .collect::<Vec<String>>()
-                .join("; ");
-
             send_message_to_channel("ik8", "step_3", text);
         }
 
         _ => (),
     }
 }
-
-// fn get_comments(owner: &str, repo: &str, pull_number: &str, review_id: &str) -> Option<String> {
-//     dotenv().ok();
-//     let api_token = env::var("GITHUB_TOKEN").expect("GITHUB_TOKEN must be set");
-
-//     let mut writer = Vec::new();
-
-//     let url = format!(
-//         "https://api.github.com/repos/{}/{}/pulls/{}/reviews/{}/comments",
-// "https://api.github.com/repos/jaykchen/vitesse-lite/pulls/1259711866/reviews/1325364967/comments",
-//         owner, repo, pull_number, review_id
-//     );
-//     let uri = Uri::try_from(&url).unwrap();
-
-//     let bearer_token = format!("Bearer {}", api_token);
-
-//     match Request::new(&uri)
-//         .method(Method::GET)
-//         .header("Authorization", &bearer_token)
-//         .header("Content-Type", "application/json")
-//         .send(&mut writer)
-//     {
-//         Ok(res) => {
-//             if !res.status_code().is_success() {
-//                 return Some(res.status_code().to_string());
-//             }
-
-//             // let raw: ChatResponse = serde_json::from_slice(&writer).unwrap();
-//             // let answer = raw.choices[0].message.content.clone();
-//             // return Some(answer);
-//             return Some(String::from_utf8(writer).unwrap());
-//         }
-//         Err(_) => {}
-//     };
-// }
